b3cb7b8066311af7bb2f0c87fe3b5a1e373e917c
peng <songpeng24@msn.com>
2020-09-05 11:03:28 +0800

    .pip/pip.conf

--- script/103-python.sh
+++ script/103-python.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+# install python and pip
+sudo apt install python3
+sudo apt install python3-pip
+# upgrade pip , make 'pip config' available
+pip3 install --index-url https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip 
+# check config list
+#python3 -m pip config list
+# config a local source
+#pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+# local config
 sudo cp -r ../overlay/home/peng/.pip ~/
