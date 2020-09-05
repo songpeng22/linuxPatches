84091b2b19161736e9755395a16ebd0a9e2036db
peng <songpeng24@msn.com>
2020-09-05 22:41:44 +0800

    sources.ustc.arm && init

--- script/001-init.sh
+++ script/001-init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt install build-essential htop tree -y
+# download tool
+sudo apt install git wget curl -y
 # ubuntu specific
 sudo apt install gnome-terminal -y
+# file content
+sudo apt install apt-file -y
 
