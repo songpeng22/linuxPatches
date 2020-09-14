fdad94c7da6cc1b45f8471091b56aaafba3c79ef
peng <songpeng24@msn.com>
2020-09-14 23:40:17 +0800

    gtags install

--- script/102-vim.sh
+++ script/102-vim.sh
@@ -2,3 +2,12 @@
 sudo apt install ctags
 sudo cp -vr ../overlay/home/peng/.vim/ ~/
 sudo cp -v  ../overlay/home/peng/.vimrc ~/.vimrc
+# gtags
+wget http://mirror.hust.edu.cn/gnu/global/global-6.6.tar.gz
+sudo tar -xvf global-6.6.tar.gz
+cd global-6.6
+./configure
+make
+sudo make install
+cd ..
+sudo rm -rf global-6.6 global-6.6.tar.gz
